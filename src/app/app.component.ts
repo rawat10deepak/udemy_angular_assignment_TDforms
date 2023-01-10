30e1119e28ccb1bcbf1b3209a66318c00ad41ffa
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-10 18:21:58 +0530

    assignment done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('myForm') subsForm: NgForm;
 
+  subsList = ['Basic', 'Advance', 'Pro'];
+  defaultSubs = 'Advance';
+
+  onSubmit() {
+    console.log(this.subsForm.value);
+  }
 }
